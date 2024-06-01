59b893c1518c442b22713ffb053c4352851a18cc
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-06-01 20:31:18 +0530

    16:55

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu-link");
+    const icon = document.querySelector(".hamburger-icon");
+
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
